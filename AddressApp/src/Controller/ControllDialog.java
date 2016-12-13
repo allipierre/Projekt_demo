efdbab6df3cf175ac87ae24dda7eb4fd98e68815
ALli Pierre <P.Yotti@atlantic-bonn.de>
2016-12-13 16:00:11 +0100

    AdressAPp

---
+++ AddressApp/src/Controller/ControllDialog.java
@@ -0,0 +1,64 @@
+package Controller;
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
+
+import application.MainApp;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
+import javafx.scene.image.Image;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class ControllDialog {
+	@FXML
+	private DatePicker DP;
+
+	public ControllDialog start() {
+		try {
+			// ModellHome m = new ModellHome();
+			// setId(m.getIdm()) ;
+			Stage primaryStage = new Stage();
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/Dialog.fxml"));
+			Parent root = loader.load();
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("bal.png")));
+			primaryStage.setScene(scene);
+
+			primaryStage.initModality(Modality.WINDOW_MODAL);
+			primaryStage.initOwner(MainApp.getPrimaryStage());
+			primaryStage.setTitle("My Dialog");
+			primaryStage.setResizable(false);
+			primaryStage.show();
+
+			return loader.getController();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@FXML
+	public void setDatePicker() {
+		DP.setShowWeekNumbers(false);
+	}
+
+	public void initialize(URL url, ResourceBundle rb) {
+		setDatePicker();
+		performSelection();
+
+	}
+	@FXML
+	public void performSelection(){
+		DP.setOnAction(event -> {
+		    LocalDate date = DP.getValue();
+		    System.out.println("Selected date: " + date);
+		});
+	}
+
+}
---
+++ AddressApp/src/Controller/PersonController.java
@@ -0,0 +1,18 @@
+package Controller;
+
+import javafx.scene.control.Alert;
+
+public class PersonController {
+
+	private void initialize() {
+		
+
+	}
+
+	public void performDialoge() {
+
+		new ControllDialog().start();
+
+	}
+
+}
